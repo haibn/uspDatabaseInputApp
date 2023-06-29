15195d15b5a76ed55b5f98e6108a31e3050a172b
haibn <haibinyuy@berkeley.edu>
2023-06-28 23:21:24 -0700

    web app finished

--- gs.js
+++ gs.js
@@ -1,10 +1,11 @@
 // Gets the main html file and runs it
 function doGet(e){
-    return HtmlService.createTemplateFromFile('index').evaluate();
-}
-
-// Helper function used to link multiple html files
-function include(filename){
+    let tmp = HtmlService.createTemplateFromFile('index');
+    return tmp.evaluate();
+  }
+  
+// Helper function used to link multiple html files (Used to link stylesheet.html and js.html to index.html)
+function include(filename){ 
     return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
 
@@ -17,6 +18,7 @@ function isInt(value) {
 // Function used to add the given infomation inputted by the user on the web app to google sheets.
 // This function is called by the addRecord() function in js.html when the "add" button is clicked.
 function addRecordToSheets(vars) {
+    console.log(vars);
     let sid = vars.sid;
     let tbb = vars.tbb;
     let intern = vars.intern;
@@ -26,7 +28,7 @@ function addRecordToSheets(vars) {
     let firebaugh = vars.firebaugh;
     let staff = vars.staff;
     Logger.log("Variables: " + [sid, tbb, intern, scholarship, emergency, undocuScholars, firebaugh, staff]);
-    let url = "https://docs.google.com/spreadsheets/d/1TrRa9c8rLXf63JnLqOF8VW2xCTo44fEbl4RyOkRi3SY/edit#gid=1510457293";
+    let url = "";
     let ss = SpreadsheetApp.openByUrl(url);
     let ws = ss.getSheetByName("Sheet1");
     let wssids = ws.getRange(2,2,ws.getLastRow(), 1).getValues();
@@ -35,8 +37,8 @@ function addRecordToSheets(vars) {
     for (let i = 0; i < wssids.length - 1; i++) {
         var wssid = wssids[i][0];
         if (sid == wssid) {
-            sidFound = true;
-            break;
+        sidFound = true;
+        break;
         }
         ite += 1;
     }
@@ -49,28 +51,89 @@ function addRecordToSheets(vars) {
     let staffCell = ws.getRange('K' + ite);
     if (sidFound) {
         if (tbbCell.getValue() == "" || (tbbCell.getValue() != tbb && isInt(tbb))) {
-            tbbCell.setValue(tbb);
+        tbbCell.setValue(tbb);
         }
         if (internCell.getValue() == "" || (internCell.getValue() != intern && isInt(intern))) {
-            internCell.setValue(intern);
+        internCell.setValue(intern);
         }
         if (scholarshipCell.getValue() == "" || (scholarshipCell.getValue() != scholarship && isInt(scholarship))) {
-            scholarshipCell.setValue(scholarship);
+        scholarshipCell.setValue(scholarship);
         }
         if (emergencyCell.getValue() == "" || (emergencyCell.getValue() != emergency && isInt(emergency))) {
-            emergencyCell.setValue(emergency);
+        emergencyCell.setValue(emergency);
         }
         if (undocuScholarsCell.getValue() == "" || (undocuScholarsCell.getValue() != undocuScholars && isInt(undocuScholars))) {
-            undocuScholarsCell.setValue(undocuScholars);
+        undocuScholarsCell.setValue(undocuScholars);
         }
         if (firebaughCell.getValue() == "" || (firebaughCell.getValue() != firebaugh && isInt(firebaugh))) {
-            firebaughCell.setValue(firebaugh);
+        firebaughCell.setValue(firebaugh);
         }
         if (staffCell.getValue() == "" || (staffCell.getValue() != staff &  staff != "")) {
-            staffCell.setValue(staff);
+        staffCell.setValue(staff);
         }
     } else {
         throw Error("sid does not exists");
     }
 }
-  
+
+// Function used to search a record in google sheets with the sid inputted by the user on the web app.
+// This function is called by the searchRecord() function in js.html when the "search" button is clicked.
+function searchRecordBySid(sid) {
+    let url = "";
+    let ss = SpreadsheetApp.openByUrl(url);
+    let ws = ss.getSheetByName("Sheet1");
+    let wssids = ws.getRange(2,2,ws.getLastRow(), 1).getValues();
+
+    let sidList = wssids.map(function(r) { return r[0]; });
+    let position = sidList.indexOf(Number(sid));
+    if (position > -1 && sid != "") {
+        position += 2;
+        let data = {
+            nameCell: ws.getRange('A' + position).getValue(),
+            sidCell: ws.getRange('B' + position).getValue(),
+            emailCell: ws.getRange('C' + position).getValue(),
+            tbbCell: ws.getRange('E' + position).getValue(),
+            internCell: ws.getRange('F' + position).getValue(),
+            scholarshipCell: ws.getRange('G' + position).getValue(),
+            emergencyCell: ws.getRange('H' + position).getValue(),
+            undocuScholarsCell: ws.getRange('I' + position).getValue(),
+            firebaughCell: ws.getRange('J' + position).getValue(),
+            staffCell: ws.getRange('K' + position).getValue()
+        }
+        Logger.log("Data: " + data);
+        return data;
+    } else {
+        throw Error("sid does not exists");
+    }
+}
+
+    // Function used to search a record in google sheets with the email inputted by the user on the web app.
+    // This function is called by the searchRecord() function in js.html when the "search" button is clicked.
+function searchRecordByEmail(email) {
+    let url = "";
+    let ss = SpreadsheetApp.openByUrl(url);
+    let ws = ss.getSheetByName("Sheet1");
+    let wsEmails = ws.getRange(2,3,ws.getLastRow(), 1).getValues();
+
+    let emailList = wsEmails.map(function(r) { return r[0]; });
+    let position = emailList.indexOf(email);
+    if (position > -1 && email != "") {
+        position += 2;
+        let data = {
+            nameCell: ws.getRange('A' + position).getValue(),
+            sidCell: ws.getRange('B' + position).getValue(),
+            emailCell: ws.getRange('C' + position).getValue(),
+            tbbCell: ws.getRange('E' + position).getValue(),
+            internCell: ws.getRange('F' + position).getValue(),
+            scholarshipCell: ws.getRange('G' + position).getValue(),
+            emergencyCell: ws.getRange('H' + position).getValue(),
+            undocuScholarsCell: ws.getRange('I' + position).getValue(),
+            firebaughCell: ws.getRange('J' + position).getValue(),
+            staffCell: ws.getRange('K' + position).getValue()
+        }
+        Logger.log("Data: " + data);
+        return data;
+    } else {
+        throw Error("sid does not exists");
+    }
+}
--- script.js
+++ script.js
@@ -1,58 +1,190 @@
-// Function used to switch between submission types.
-function submissionType() {
-  var type = document.getElementById("typeOfSubmission");
-  var awards = document.getElementById("awards");
-  var appointments = document.getElementById("appointments");
-  var form = document.getElementsByClassName("form");
-  console.log(type);
-  if (type.value === "awards") {
-    appointments.style.display = "none";
-    awards.style.display = "block";
-    form[0].style.height = "500px";
-  } else {
-    awards.style.display = "none";
-    appointments.style.display = "block";
-    form[0].style.height = "420px";
+  //Update Student Information button. When clicked, it displays the form. 
+  function updateForm() {
+    var home = document.getElementById("home");
+    var form = document.getElementById("update");
+    home.style.display = "none";
+    form.style.display = "block";
   }
-}
 
-// Sets up the initial state of the web app when the DOM is loaded.
-document.addEventListener("DOMContentLoaded", function(event) {
-  var awards = document.getElementById("awards");
-  awards.style.display = "block";
-});
-
-// Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
-function noSid() {
-  alert("SID not found.");
-}
-
-// Function used to add the given infomation inputted by the user on the web app to google sheets.
-// This function is triggered when the "add" button is clicked.
-function addRecord() {
-  let typeOfSubmission = document.getElementById("typeOfSubmission").value;
-  let vars = {
-    sid: document.getElementById("sid").value,
-    tbb: "",
-    intern: "",
-    scholarship: "",
-    emergency: "",
-    undocuScholars: "",
-    firebaugh: "",
-    staff: ""
+  //Search Student Information button. When clicked, it displays the form. 
+  function searchForm() {
+    var home = document.getElementById("home");
+    var form = document.getElementById("search");
+    home.style.display = "none";
+    form.style.display = "block";
   }
-  if (typeOfSubmission == "awards") {
-    let awardType = document.getElementById("typeOfAward").value;
-    let amount = document.getElementById("amount").value;
-    for (var key in vars) {
-      if (awardType == key) {
-        vars[key] = amount;
-      }
+
+  // Back button. When clicked, it returns to the home page
+  function back() {
+    var home = document.getElementById("home");
+    var updateForm = document.getElementById("update");
+    var searchForm = document.getElementById("search");
+    let summary = document.getElementById("summary");
+
+    if (updateForm.style.display == "block") {
+      home.style.display = "block";
+      updateForm.style.display = "none";
+    } else if (summary.style.display == "block") {
+      home.style.display = "block";
+      summary.style.display = "none";
+      searchForm.style.display = "none";
+    } else {
+      home.style.display = "block";
+      searchForm.style.display = "none";
     }
-  } else {
-    vars.staff = document.getElementById("staff").value;
   }
-  google.script.run.withFailureHandler(noSid).addRecordToSheets(vars);
-  document.getElementById("sid").value = "";
-  document.getElementById("amount").value = "";
-}
+
+  // Function used to switch between search types.
+  function searchType() {
+    var type = document.getElementById("searchType");
+    var sid = document.getElementById("sidInput");
+    var uid = document.getElementById("uidInput");
+    var email = document.getElementById("emailInput");
+    if (type.value == "sid") {
+      sid.style.display = "block";
+      uid.style.display = "none";
+      email.style.display = "none";
+    } else if (type.value == "uid") {
+      sid.style.display = "none";
+      uid.style.display = "block";
+      email.style.display = "none";
+    } else {
+      sid.style.display = "none";
+      uid.style.display = "none";
+      email.style.display = "block";
+    }
+  }
+
+  // Function called by recordFound(data)
+  function informationSumm() {
+    let summary = document.getElementById("summary");
+    let form = document.getElementById("search");
+    form.style.display = "none";
+    summary.style.display = "block";
+  }
+
+  // If record in found in the database (google sheets), retrieves the information 
+  // and displays it in the web app by calling informationSumm()
+  function recordFound(data) {
+    if (data.internCell != "") {
+      document.getElementById("summInternBox").innerHTML = "&#9989;";
+    } else {
+      document.getElementById("summInternBox").innerHTML = "&#10060;";
+    }
+    if (data.tbbCell != "") {
+      document.getElementById("summTbbBox").innerHTML = "&#9989;";
+    } else {
+      document.getElementById("summTbbBox").innerHTML = "&#10060;";
+    }
+    if (data.emergencyCell != "") {
+      document.getElementById("summEmergencyBox").innerHTML = "&#9989;";
+    } else {
+      document.getElementById("summEmergencyBox").innerHTML = "&#10060;";
+    }
+    if (data.scholarshipCell != "") {
+      document.getElementById("summScholarshipBox").innerHTML = "&#9989;";
+    } else {
+      document.getElementById("summScholarshipBox").innerHTML = "&#10060;";
+    }
+    if (data.undocuScholarsCell != "") {
+      document.getElementById("summUndocuBox").innerHTML = "&#9989;";
+    } else {
+      document.getElementById("summUndocuBox").innerHTML = "&#10060;";
+    }
+    if (data.firebaughCell != "") {
+      document.getElementById("summFirebaughBox").innerHTML = "&#9989;";
+    } else {
+      document.getElementById("summFirebaughBox").innerHTML = "&#10060;";
+    }
+    document.getElementById("summName").innerHTML = data.nameCell;
+    document.getElementById("summSid").innerHTML = data.sidCell;
+    document.getElementById("summUid").innerHTML = "n/a";
+    document.getElementById("summIntern").innerHTML = "$" + data.internCell;
+    document.getElementById("summTbb").innerHTML = "$" + data.tbbCell;
+    document.getElementById("summEmergency").innerHTML = "$" + data.emergencyCell;
+    document.getElementById("summScholarship").innerHTML = "$" + data.scholarshipCell;
+    document.getElementById("summUndocu").innerHTML = "$" + data.undocuScholarsCell;
+    document.getElementById("summFirebaugh").innerHTML = "$" + data.firebaughCell;
+    document.getElementById("summStaff").innerHTML = data.staffCell;
+    informationSumm();
+  }
+
+  // Function used to search a specific record based on its search type inputted by the user on the web app from google sheets.
+  // This function is triggered when the "search" button is clicked.
+  function searchRecord() {
+    let type = document.getElementById("searchType");
+    let sid = document.getElementById("sidSearch");
+    let uid = document.getElementById("uidSearch");
+    let email = document.getElementById("emailSearch");
+    if (type.value == "sid") {
+      google.script.run.withSuccessHandler(recordFound).withFailureHandler(noSid).searchRecordBySid(sid.value);
+      sid.value = "";
+    } else if (type.value == "uid") {
+      google.script.run.searchRecordByUid(uid.value);
+    } else {
+      google.script.run.withSuccessHandler(recordFound).withFailureHandler(noSid).searchRecordByEmail(email.value);
+      email.value = "";
+    }
+  }
+
+  // Function used to switch between submission types.
+  function submissionType() {
+    var type = document.getElementById("typeOfSubmission");
+    var awards = document.getElementById("awards");
+    var appointments = document.getElementById("appointments");
+    if (type.value == "awards") {
+      appointments.style.display = "none";
+      awards.style.display = "block";
+    } else {
+      awards.style.display = "none";
+      appointments.style.display = "block";
+    }
+  }
+
+  // Sets up the initial state of the web app when the DOM is loaded.
+  // document.addEventListener("DOMContentLoaded", function(event) {
+  //   var awards = document.getElementById("awards");
+  //   awards.style.display = "block";
+  // });
+
+  // Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
+  function noSid() {
+    alert("Record not found.");
+  }
+
+  // Helper function used to alert the user if the inputted SID is not found in the database (google sheets)
+  function recordAdded() {
+    document.getElementById("sid").value = "";
+    document.getElementById("amount").value = "";
+  }
+
+  // Function used to add the given infomation inputted by the user on the web app to google sheets.
+  // This function is triggered when the "add" button is clicked.
+  function addRecord() {
+    let typeOfSubmission = document.getElementById("typeOfSubmission").value;
+    let vars = {
+      sid: document.getElementById("sid").value,
+      tbb: "",
+      intern: "",
+      scholarship: "",
+      emergency: "",
+      undocuScholars: "",
+      firebaugh: "",
+      staff: ""
+    }
+    if (typeOfSubmission == "awards") {
+      let awardType = document.getElementById("typeOfAward").value;
+      let amount = document.getElementById("amount").value;
+      for (var key in vars) {
+        if (awardType == key) {
+          vars[key] = amount;
+        }
+      }
+    } else {
+      vars.staff = document.getElementById("staff").value;
+    }
+    // google.script.run.withSuccessHandler(recordAdded).withFailureHandler(noSid).addRecordToSheets(vars);
+    google.script.run.withFailureHandler(noSid).addRecordToSheets(vars);
+    document.getElementById("sid").value = "";
+    document.getElementById("amount").value = "";
+  }
